156e2bc975b9dfed95986f42c7b987cc8459a8fa
Numix <92272552+NumixDev@users.noreply.github.com>
2022-07-31 14:39:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -33,9 +33,31 @@ listSnake = []
 
 inicialSize = 5
 
+died = False
+
 def inscreaseSnakeSize(listSnake):
     for XandY in listSnake:
         pygame.draw.rect(screen, (0, 255, 0), (XandY[0], XandY[1], 20, 20))
+
+def restartGame():
+
+    global apples, xApple, yApple, inicialSize, xSnake, ySnake, listSnake, listHead, died
+
+    # Apples
+
+    apples = 0
+    xApple = randint(40, 600)
+    yApple = randint(50, 430)
+
+    # Snake
+
+    inicialSize = 5
+    xSnake = xScreen/2
+    ySnake = yScreen/2
+    listSnake = []
+    listHead = []
+
+    died = False
 
 while True:
     clock.tick(60)
@@ -48,7 +70,7 @@ while True:
             pygame.quit()
             exit()
     
-    # new movement
+    # New movement
 
         if event.type == KEYDOWN:
             if event.key == K_a:
@@ -94,6 +116,35 @@ while True:
     listHead.append(ySnake)
 
     listSnake.append(listHead)
+
+    if listSnake.count(listHead) > 1:
+        font2 = pygame.font.SysFont('arial', 20, True, False)
+        message = 'Game Over! Press R to restart the Game'
+        formatedText = font2.render(message, True, (255, 255, 255))
+        rectText = formatedText.get_rect()
+        died = True
+        while died:
+            screen.fill((0, 0, 0))
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    exit
+                if event.type == KEYDOWN:
+                    if event.key == K_r:
+                        restartGame()
+
+            rectText.center = (xScreen//2, yScreen//2)
+            screen.blit(formatedText, rectText)
+            pygame.display.update()
+
+    if xSnake > xScreen:
+        xSnake = 0
+    if xSnake < 0:
+        xSnake = xScreen
+    if ySnake < 0:
+        ySnake = yScreen
+    if ySnake > yScreen:
+        ySnake = 0
 
     if len(listSnake) > inicialSize:
         del listSnake[0]
